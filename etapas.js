5347c18b528af0daded3370cf7aa1da796be6307
Dyego Castro <90106463+Nightalone@users.noreply.github.com>
2022-06-17 15:50:07 -0300

    Commit inicial
    Primeiro commit

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+        {
+            numero: '12345',
+            nome: 'Gohan',
+            partido: 'ABC',
+            fotos:[
+                {url:'gohan.jpeg', legenda: 'Vereador'}
+            ]
+        },
+        {
+            numero: '54321',
+            nome: 'Picolo',
+            partido: 'DEFG',
+            fotos:[
+                {url:'picolo.jpeg', legenda: 'Vereador'}
+            ]
+        },
+        ] 
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '12',
+                nome: 'Goku',
+                partido: 'ABC',
+                vice: 'Goten',
+                fotos:[
+                    {url:'goku.jpg', legenda: 'Prefeito'},
+                    {url: 'goten.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '21',
+                nome: 'Vegeta',
+                partido: 'QWERTY',
+                vice: 'Trunks',
+                fotos:[
+                    {url:'vegeta.jpg', legenda: 'Prefeito'},
+                    {url: 'trunks.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]    
+    }
+];
